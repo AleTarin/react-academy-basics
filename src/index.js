6a5fd327108bce65cdbe9329186c254184783757
de la Cruz, Alejandro <a.de.la.cruz@accenture.com>
2019-01-28 15:13:14 -0600

    Added Guide

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Title from './Title';
-import Card from './Card';
-import './style.css';
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App';
 
-
-const url = <a href="https://reactjs.org/docs/hello-world.html">React Documentation</a>;
-
-// Class component
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  // Render life cylcle
-  render() {
-    const article = {
-        title: 'Prop Propagation',
-        list: 
-        [ 
-          "Child: Props",
-          "Parent: Emit Event"
-        ],
-        img: 'https://static.imasters.com.br/wp-content/uploads/2017/06/4-1.png'
-    }
-
-    return (
-      <main className="wrapper">
-        <Title name={this.state.name} />
-        <Card title={article.title.toUpperCase()} list={article.list} img={article.img}/>
-        <p>
-          Based on: {url}
-        </p>
-      </main>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(
+  <App />, 
+  document.getElementById('root')
+);
